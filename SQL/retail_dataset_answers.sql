01fe95b04cbfcf8ba9ea505cb67d81f65878ceba
Thuto <ThutoSiya@gmail.com>
2026-03-08 18:57:33 +0200

    Section 3 complete

--- SQL/retail_dataset_answers.sql
+++ SQL/retail_dataset_answers.sql
@@ -26,3 +26,38 @@ FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
 
 SELECT DISTINCT(Gender)
 FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+
+--3. WHERE Clause
+
+--Q5. Display all transactions where the Age is greater than 40. Expected output: All columns
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+WHERE Age > 40
+
+--Q6. Display all transactions where the Price per Unit is between 100 and 500. Expected output: All columns
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+WHERE Price_per_Unit BETWEEN 100 AND 500
+
+--Q7. Display all transactions where the Product Category is either 'Beauty' or 'Electronics'. Expected output: All columns
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+WHERE Product_Category IN ('Beauty' , 'Electronics')
+
+--Q8. Display all transactions where the Product Category is not 'Clothing'. Expected output: All columns
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+WHERE Product_Category  NOT IN ('Clothing')
+
+--Q9. Display all transactions where the Quantity is greater than or equal to 3. Expected output: All columns
+
+SELECT *
+FROM retail_sales.dbo.[1772863888723_retail_sales_dataset]
+WHERE Quantity >= 3
